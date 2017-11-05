914618e1ce7e6d64031e716d4a4c9b2e6439655e
mynikolas <took.out.the.stars@gmail.com>
2017-11-05 18:02:44 +0200

    Content section with adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -8,7 +8,7 @@ $(function() {
 	offCanvas: {
 		position: 'right'
 	}
-	});
+});
 
 	var api = $('#my-menu').data('mmenu');
 	api.bind('open:start', function() {
@@ -17,5 +17,59 @@ $(function() {
 	api.bind('close:start', function(){
 		$('.hamburger').removeClass('is-active');
 	});
+	
+	$('.carousel-service-composition h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+	
+	$('section h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+	});
+	
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carouselService()
+		}, 200);
+	});
+	
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		dots: false,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			800: {
+				items: 2,
+			},
+			1100: {
+				items: 3,
+			}
+		}
+	}).on('changed.owl.carousel', function() {
+		carouselService();
+	});
+		
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths  = $(this);
+					thsh = ths.find('.carousel-services-content').outerHeight();
+			ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	}carouselService();
+	
+	//Resize Window
+	function onResize() {
+		$('.carousel-services-content').equalHeights();
+	}onResize();
+	window.onresize = function() {
+		onResize()
+	};
 	
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,8 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
+		'app/libs/equalHeights/equalheights.js',
+		'app/libs/owl.carousel/owl.carousel.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
